cdc37fbabb7fa86ce6882bfa962a95f4622784d0
spoorthi <spoorthi@csgkarnataka.in>
2024-05-23 18:21:11 +0530

    added api

--- src/assets/Components/Search/Search.jsx
+++ src/assets/Components/Search/Search.jsx
@@ -24,26 +24,52 @@
 import React, { useState } from 'react';
 import searchIcon from '../../Images/search.png';
 import './Search.css';
+import axios from 'axios';
 
 const Search = () => {
-  const [dateValue, setDateValue] = useState(null);
-
-  const handleDateChange = (date) => {
-    setDateValue(date.value);
+  const [cityName, setCityName] = useState('');
+  const searchCity = () => {
+    const url = 'http://api.weatherapi.com/v1/current.json?key=f2cc0a0e73ec4eb7b4955211242005'
+    const params = {
+      q:cityName
+    }
+    axios 
+    .get(url, {params})
+    .then(response =>{
+      console.log(response.data)
+    
+    })
+    .catch(error =>{console.log(error)}
+    )
   };
-
+  const selectedDate = (e) => {
+    const url = 'http://api.weatherapi.com/v1/history.json?key=f2cc0a0e73ec4eb7b4955211242005'
+    const params={
+      q:cityName,
+      dt: e.target.value
+    }
+    axios
+    .get(url, {params})
+    .then(response =>{
+      console.log(response.data)
+    })
+    .catch(error =>{console.log(error)}
+    )
+  }
   return (
+    
     <div className="searchBlock m-2 d-flex justify-content-center">
         <div>
 
       <div className="searchcityDiv d-flex">
-        <input className="searchBar m-2 p-2" type="text" placeholder="Search for city" />
-        <button className="searchButton my-2">
+        <input className="searchBar m-2 p-2" type="text" placeholder="Search for city" value={cityName}
+            onChange={(e) => setCityName(e.target.value)}/>
+        <button className="searchButton my-2" onClick={searchCity}>
           <img className="searchIcon" src={searchIcon} alt="search icon" />
         </button>
       </div>
       <div className="searchDate m-2">
-       <input className='dateBar p-2' type="date" placeholder='YYYY-MM-DD' format ='YYYY-MM-DD'/>
+       <input className='dateBar p-2' type="date" placeholder='YYYY-MM-DD' format ='YYYY-MM-DD' onChange={selectedDate}/>
       </div>
         </div>
     </div>
